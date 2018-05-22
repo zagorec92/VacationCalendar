d533a9641c2a7f0af372758efe9705fe58c76a1d
Ivan <zagorec92@users.noreply.github.com>
2018-05-22 09:53:17 +0200

    Extra whitespace removed and email fix

--- API/App.DAL/AppDbContext.cs
+++ API/App.DAL/AppDbContext.cs
@@ -78,12 +78,12 @@ namespace App.DAL
 				new User() { ID = 1, FirstName = "Admin1", LastName = null, Email = "admin1@ec.com", Password = "x61Ey612Kl2gpFL56FT9weDnpSo4AV8j8+qx2AuTHdRyY036xxzTTrw10Wq3+4qQyB+XURPWx1ONxp3Y3pB37A==", Active = true, DateCreated = DateTime.Now },
 				new User() { ID = 2, FirstName = "Admin2", LastName = null, Email = "admin2@ec.com", Password = "x61Ey612Kl2gpFL56FT9weDnpSo4AV8j8+qx2AuTHdRyY036xxzTTrw10Wq3+4qQyB+XURPWx1ONxp3Y3pB37A==", Active = true, DateCreated = DateTime.Now },
 				new User() { ID = 3, FirstName = "Martin", LastName = "Oliver", Email = "martin.oliver@ec.com", Password = "voHa7tBTzLqzhskUuRtanltSu+yVUb7bN+nu2YEylI0r9I97ab6FBbFGasaP5gvhNh7xsHjmnG9MwhMGOGvbeg==", Active = true, DateCreated = DateTime.Now },
-				new User() { ID = 4, FirstName = "Christopher ", LastName = "Johnson", Email = "christopher.johnson@ec.com", Password = "LBlU1yhdXop/N50+PTh4ACbfj8ZPevCerWCF3QBSUnmosfIOXpMebDnfxK1Zj9B2OeJnQl9Ds6PW61hTJoj8Ww==", Active = true, DateCreated = DateTime.Now },
-				new User() { ID = 5, FirstName = "Richard ", LastName = "Jackson", Email = "richard.jackson@ec.com", Password = "ZEZzRC/6axBx6TMXhBftfXX/C3kPzeWlKyaehMweevTxHzDhbqd+iRuyZsbwk9zIfmS1zYk6EznXE/kPRWr8Iw==", Active = true, DateCreated = DateTime.Now },
-				new User() { ID = 6, FirstName = "Jessica ", LastName = "Stratton", Email = "jessica.stratton@ec.com", Password = "p0dzcANjeuT5Ggybu1x/lSivzXu/d4MvvPWI2/fLK4TNoGeaQ+QueZ8q1XYGsX+OI24FFfhx7VIVOmSCc8Ytfw==", Active = true, DateCreated = DateTime.Now },
+				new User() { ID = 4, FirstName = "Christopher", LastName = "Johnson", Email = "christopher.johnson@ec.com", Password = "LBlU1yhdXop/N50+PTh4ACbfj8ZPevCerWCF3QBSUnmosfIOXpMebDnfxK1Zj9B2OeJnQl9Ds6PW61hTJoj8Ww==", Active = true, DateCreated = DateTime.Now },
+				new User() { ID = 5, FirstName = "Richard", LastName = "Jackson", Email = "richard.jackson@ec.com", Password = "ZEZzRC/6axBx6TMXhBftfXX/C3kPzeWlKyaehMweevTxHzDhbqd+iRuyZsbwk9zIfmS1zYk6EznXE/kPRWr8Iw==", Active = true, DateCreated = DateTime.Now },
+				new User() { ID = 6, FirstName = "Jessica", LastName = "Stratton", Email = "jessica.stratton@ec.com", Password = "p0dzcANjeuT5Ggybu1x/lSivzXu/d4MvvPWI2/fLK4TNoGeaQ+QueZ8q1XYGsX+OI24FFfhx7VIVOmSCc8Ytfw==", Active = true, DateCreated = DateTime.Now },
 				new User() { ID = 7, FirstName = "Frank", LastName = "Boehm", Email = "frank.boehm@ec.com", Password = "GbcCtvGxNcoAnOHfE9hHOLdMt0Ra9j3Tby/e/nffg/zezzB9tTYJNSgkLDzlKpNQRVrLHorZBvzNQP0JWrq08Q==", Active = true, DateCreated = DateTime.Now },
-				new User() { ID = 8, FirstName = "Joann ", LastName = "Camp", Email = "joan.camp@ec.com", Password = "47Re7pm1d0izINaLr8gpNtNOO8F2obGGlhcduinHgqqNXgsaDjXtaU8GcLjaueeHnIynB+UcNi0cGqNqO1DRXg==", Active = true, DateCreated = DateTime.Now },
-				new User() { ID = 9, FirstName = "Julie ", LastName = "Burroughs", Email = "julie.burroughs@ec.com", Password = "7qrgzXN9I2na3BLoFVDGJovOlWI28WEPfg2NA85lIglFmArxAL7pwQfU0IStmrA6Q1KnCoOGDOCuA9ev46RsAw==", Active = true, DateCreated = DateTime.Now }
+				new User() { ID = 8, FirstName = "Joann", LastName = "Camp", Email = "joann.camp@ec.com", Password = "47Re7pm1d0izINaLr8gpNtNOO8F2obGGlhcduinHgqqNXgsaDjXtaU8GcLjaueeHnIynB+UcNi0cGqNqO1DRXg==", Active = true, DateCreated = DateTime.Now },
+				new User() { ID = 9, FirstName = "Julie", LastName = "Burroughs", Email = "julie.burroughs@ec.com", Password = "7qrgzXN9I2na3BLoFVDGJovOlWI28WEPfg2NA85lIglFmArxAL7pwQfU0IStmrA6Q1KnCoOGDOCuA9ev46RsAw==", Active = true, DateCreated = DateTime.Now }
 			);
 
 			modelBuilder.Entity<Role>().HasData(
